c6237e7cd1351d282b0ff5d5a6d60b689f74404c
bakhwee-bug <parksy8103@gmail.com>
2024-06-20 19:27:05 +0900

    [feat] 녹음 저장할 때 파일 명 바꾸기

--- flutter_application_sync/lib/view/audioPlayerPage.dart
+++ flutter_application_sync/lib/view/audioPlayerPage.dart
@@ -1,6 +1,11 @@
-import 'package:Sync/const/colors.dart';
+import 'package:Sync/view/audioRecoderView.dart'; // Record 화면 경로에 맞게 수정
+import 'package:Sync/view/myView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:Sync/const/colors.dart';
+import 'package:Sync/const/styles.dart';
+import 'dart:io';
 
 class AudioPlayerPage extends StatefulWidget {
   final String recordingPath;
@@ -61,6 +66,27 @@ class _AudioPlayerPageState extends State<AudioPlayerPage> {
     }
   }
 
+  Future<void> _saveRecording() async {
+    try {
+      // 노래 ID를 사용하여 파일 이름 생성
+      String newPath = '/sdcard/Download/song_id.wav';
+      File(widget.recordingPath).copySync(newPath);
+      File(widget.recordingPath).deleteSync();
+
+      // MyView 화면으로 이동
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => MyView(),
+        ),
+      );
+    } catch (e) {
+      print('Error saving recording: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error saving recording: $e')),
+      );
+    }
+  }
+
   @override
   void dispose() {
     _player!.closePlayer();
@@ -92,6 +118,16 @@ class _AudioPlayerPageState extends State<AudioPlayerPage> {
             ElevatedButton(
               onPressed: _isPlaying ? _stopPlaying : _startPlaying,
               child: Text(_isPlaying ? 'Stop Playing' : 'Play Recording'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _saveRecording,
+              child: Text('Save Recording'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Record Again'),
             ),
           ],
         ),
